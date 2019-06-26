61a30351a54e30331822b27b068474b84146a1d4
LZ <liuzhellll@hotmail.com>
2019-06-26 18:02:08 +0800

    11

--- SchoolBusWXWeb/Repository/Common/RepositoryBase.cs
+++ SchoolBusWXWeb/Repository/Common/RepositoryBase.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-using Dapper;
+﻿using Dapper;
 using SchoolBusWXWeb.Models;
 using SchoolBusWXWeb.Utilities.PageHelper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading.Tasks;
 
 namespace SchoolBusWXWeb.Repository.Common
 {
