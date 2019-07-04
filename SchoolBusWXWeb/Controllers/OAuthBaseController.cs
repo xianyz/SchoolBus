9d1c032da62153cbbef842ab851a28fdccd6a9de
LZ <liuzhellll@hotmail.com>
2019-07-04 22:30:40 +0800

    123

--- SchoolBusWXWeb/Controllers/OAuthBaseController.cs
+++ SchoolBusWXWeb/Controllers/OAuthBaseController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
 using SchoolBusWXWeb.Filters;
-using SchoolBusWXWeb.Models.ViewData;
 
 namespace SchoolBusWXWeb.Controllers
 {
