2885c393692b0b8fe37fd66d5e2917b2972c73a0
LZ <liuzhellll@hotmail.com>
2019-07-07 16:12:56 +0800

    增加跳转页

---
+++ SchoolBusWXWeb/Controllers/SchoolBusHomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+#if !DEBUG
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using SchoolBusWXWeb.Filters;
+#endif
+using SchoolBusWXWeb.Business;
+using SchoolBusWXWeb.Models.ViewData;
+using Senparc.Weixin.MP.AdvancedAPIs.OAuth;
+
+namespace SchoolBusWXWeb.Controllers
+{
+#if !DEBUG
+    [CustomOAuth(null, "/OAuth2/UserInfoCallback")]
+#endif
+    public class SchoolBusHomeController : Controller
+    {
+        private readonly ISchoolBusBusines _schoolBusBusines;
+        private OAuthAccessTokenResult TokenResult { get; set; }
+        public SchoolBusHomeController(ISchoolBusBusines schoolBusBusines)
+        {
+            _schoolBusBusines = schoolBusBusines;
+#if !DEBUG
+          TokenResult = JsonConvert.DeserializeObject<OAuthAccessTokenResult>(HttpContext.Session.GetString("OAuthAccessTokenResult"));
+#endif
+        }
+        public async Task<IActionResult> Index(int type)
+        {
+#if DEBUG
+            TokenResult = new OAuthAccessTokenResult { openid = "oBcNx1lHzHxIpKm5m64XX99zTMGs" };
+#endif
+            var code = await _schoolBusBusines.GetUserCodeAsync(TokenResult.openid);
+            return View(new IndexModel { type = type, code = code });
+        }
+    }
+}
