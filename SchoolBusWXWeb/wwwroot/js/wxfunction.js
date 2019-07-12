d3c3d5702d65d737991c46cb3c724ebcab062f58
LZ <liuzhellll@hotmail.com>
2019-07-13 00:24:53 +0800

    修改分享

--- SchoolBusWXWeb/wwwroot/js/wxfunction.js
+++ SchoolBusWXWeb/wwwroot/js/wxfunction.js
@@ -7,7 +7,7 @@ wx.ready(function () {
     // 转发到朋友圈
     wx.onMenuShareTimeline({
         title: '鲸卫士校车联盟-绑定乘车卡,关注孩子乘车安全',
-        link: 'http://wx.360wll.cn/SchoolBusHome/index?type=0',
+        link: 'http://wx.360wll.cn/SchoolBus/Index?type=0',
         imgUrl: 'http://wx.360wll.cn/img/pic1.jpg',
         success: function () {
             console.log("转发到朋友圈成功");
@@ -20,7 +20,7 @@ wx.ready(function () {
     wx.onMenuShareAppMessage({
         title: '鲸卫士校车联盟',
         desc: '绑定乘车卡,关注孩子乘车安全',
-        link: 'http://wx.360wll.cn/SchoolBusHome/index?type=0',
+        link: 'http://wx.360wll.cn/SchoolBus/Index?type=0',
         imgUrl: 'http://wx.360wll.cn/img/pic1.jpg',
         type: '',
         dataUrl: '',
